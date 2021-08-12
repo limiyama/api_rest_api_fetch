067339ab596beb11366dd437869670b60bc3caf0
mimiyama <lmizuyama@gmail.com>
2021-08-13 06:35:58 +0900

    a

--- www/lib/script.js
+++ www/lib/script.js
@@ -1,5 +1,17 @@
 // This is a JavaScript file
 
+
+function verConexao() {
+    var networkState = navigator.connection.type;
+
+    if (states[Connection.NONE]){
+      function sair(buttonIndex){
+        navigator.app.exitApp();
+      }
+      navigator.notification.confirm("Erro de Conexão",sair,"Você está sem conexão, deixe mais tarde.",['Sair','Ficar'])
+    }
+}
+
 window.onload = function(){
   const cadastrar = document.querySelector("#cadastrar");
   const nome = document.querySelector("#nome");
@@ -10,6 +22,7 @@ window.onload = function(){
   const id = document.querySelector("#id");
 
   cadastrar.addEventListener("click", function (){
+    verConexao();
     let formdata = new Formdata();
     formdata.append('nome', `${nome.value}`);
     formdata.append('curso', `${curso.value}`);
@@ -24,32 +37,34 @@ window.onload = function(){
        limparCampos();
     }
   );
-});
+})
 
-  buscar.addEventListener("click", function(){
-    fetch (`http://www.ligiamizuyama.com.br/exemplo_api/pessoa/${id.value}`, {
-      method: "get",
-      mode: 'cors',
-      cache: 'default',
-    }).then(response=> {
-      response.json().then(data => {
-        nome.value = data['nome'];
-        curso.value = data['curso'];
-      })
+buscar.addEventListener("click", function(){
+  verConexao();
+  fetch (`http://www.ligiamizuyama.com.br/exemplo_api/pessoa/${id.value}`, {
+    method: "get",
+    mode: 'cors',
+    cache: 'default',
+  }).then(response=> {
+    response.json().then(data => {
+      nome.value = data['nome'];
+      curso.value = data['curso'];
     })
   })
+})
 
-  alterar.addEventListener("click", function(){
-    fetch (`http://www.ligiamizuyama.com.br/exemplo_api/pessoa/${id.value}`, {
-      method:"put",
-      mode:'cors',
-      cache:'default',
-      headers:{
-        'Content-type':'application/json; charset=UTF-8'
-      },
-      body:JSON.stringify({
-        'nome':`${nome.value}`,
-        'curso':`${curso.value}`,
+alterar.addEventListener("click", function(){
+  verConexao();
+  fetch (`http://www.ligiamizuyama.com.br/exemplo_api/pessoa/${id.value}`, {
+    method:"put",
+    mode:'cors',
+    cache:'default',
+    headers:{
+    'Content-type':'application/json; charset=UTF-8'
+  },
+    body:JSON.stringify({
+    'nome':`${nome.value}`,
+    'curso':`${curso.value}`,
     })
   }).then(() => {
     alert("Registro Alterado com Sucesso");
@@ -57,19 +72,52 @@ window.onload = function(){
   });
 })
 
-  deletar.addEventListener("click", function(){
-    fetch (`http://www.ligiamizuyama.com.br/exemplo_api/pessoa/${id.value}`, {
-      method:"get",
-      mode:'cors',
-      cache:'default'
-    }).then(() => {
-      alert("Registro Alterado com Sucesso");
-      limparCampos();
-    });
-  })
+deletar.addEventListener("click", function(){
+  verConexao();
+  fetch (`http://www.ligiamizuyama.com.br/exemplo_api/pessoa/${id.value}`, {
+    method:"get",
+    mode:'cors',
+    cache:'default'
+  }).then(() => {
+    alert("Registro Alterado com Sucesso");
+    limparCampos();
+  });
+})
 
 function limparCampos(){
     nome.value="";
     curso.value="";
   }
 }
+
+$(document).on("click", "#codigo", function(){
+  verConexao();
+  cordova.plugins.barcodeScanner.scan( function (result) {
+    alert("Código de barras \n" +
+        "Resultado: " + result.text + "\n" +
+        "Formato: " + result.format + "\n" +
+        "Cancelado: " + result.cancelled);
+  },
+  function (error) {
+    alert("Erro: " + error);
+  },
+  {
+    preferFrontCamera : false, 
+    showFlipCameraButton : true, 
+    showTorchButton : true,
+    torchOn: true,
+    saveHistory: true, 
+    prompt : "Coloque o código de barras na tela", 
+    resultDisplayDuration: 500, 
+    formats : "QR_CODE,PDF_417", 
+    orientation : "landscape",
+    disableAnimations : true, 
+    disableSuccessBeep: false 
+  }
+)
+});
+
+$(document).on("click", "#buscar", function(){
+  verConexao();
+});
+
